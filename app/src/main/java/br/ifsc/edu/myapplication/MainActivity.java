b046748c8b00ea7c2c9a5d5467dd3fdcc8f17698
ViniciusZinAlves <viniciuszinalves@gmail.com>
2020-03-12 10:41:57 -0300

    IMCFinalizado

--- app/src/main/java/br/ifsc/edu/myapplication/MainActivity.java
+++ app/src/main/java/br/ifsc/edu/myapplication/MainActivity.java
@@ -17,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().setTitle("Calcular IMC");
 
         txtAltura = (EditText)findViewById(R.id.txtAltura);
         txtSituacaoPessoa = (TextView)findViewById(R.id.txtSituacaoPeso);
@@ -28,32 +27,43 @@ public class MainActivity extends AppCompatActivity {
 
     public void calcularImc(View v) {
         float peso = 0, altura = 0, imc = 0;
-        if ((txtPeso.getText().toString().length() > 0) && (txtAltura.getText().toString().length() > 0)) {
+
+        if ((txtPeso.getText().toString().length() > 0) && (txtAltura.getText().toString().length() > 0))
+        {
             peso = Float.parseFloat(txtPeso.getText().toString());
             altura = Float.parseFloat(txtAltura.getText().toString());
-            if ((peso > 0) && (altura > 0)) {
+
+            if ((peso > 0) && (altura > 0))
+            {
                 imc = (peso / (altura * altura));
-                if (imc <= 18.5) {
+                if (imc <= 18.5)
+                {
                     imgResultadoImc.setImageResource(R.drawable.abaixopeso);
                     txtSituacaoPessoa.setText("Abaixo do Peso");
-                } else if ((imc >= 18.6) && (imc <= 24.9)) {
+                } else if (imc <= 24.9)
+                {
                     imgResultadoImc.setImageResource(R.drawable.normal);
                     txtSituacaoPessoa.setText("Peso Normal");
-                } else if ((imc >= 25.0) && (imc <= 29.9)) {
+                } else if (imc <= 29.9)
+                {
                     imgResultadoImc.setImageResource(R.drawable.sobrepeso);
                     txtSituacaoPessoa.setText("SobrePeso");
-                } else if ((imc >= 30.0) && (imc <= 34.9)) {
+                } else if (imc <= 34.9)
+                {
                     imgResultadoImc.setImageResource(R.drawable.obesidade1);
                     txtSituacaoPessoa.setText("Obesidade Tipo1");
-                } else if ((imc >= 35.0) && (imc <= 39.9)) {
+                } else if (imc <= 39.9)
+                {
                     imgResultadoImc.setImageResource(R.drawable.obesidade2);
                     txtSituacaoPessoa.setText("Obesidade Tipo2");
-                } else if (imc >= 40.0) {
+                } else
+                {
                     imgResultadoImc.setImageResource(R.drawable.obesidade3);
                     txtSituacaoPessoa.setText("Obesidade Tipo3");
                 }
             }
-
+            else
+                txtSituacaoPessoa.setText("Informações incorretas");
         }
     }
 }
