82cb52b9dd3be19a5cf59493bef9b4539540e543
ViniciusZinAlves <viniciuszinalves@gmail.com>
2020-03-12 11:16:32 -0300

    IMCFinalizado

--- app/src/main/java/br/ifsc/edu/myapplication/MainActivity.java
+++ app/src/main/java/br/ifsc/edu/myapplication/MainActivity.java
@@ -10,49 +10,51 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText txtPeso,txtAltura;
-    ImageView imgResultadoImc;
-    TextView txtSituacaoPessoa;
+    EditText  txtPeso          ;
+    EditText  txtAltura        ;
+    ImageView imgResultadoImc  ;
+    TextView  txtSituacaoPessoa;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtAltura = (EditText)findViewById(R.id.txtAltura);
-        txtSituacaoPessoa = (TextView)findViewById(R.id.txtSituacaoPeso);
-        txtPeso = (EditText)findViewById(R.id.txtPeso);
-        imgResultadoImc = (ImageView)findViewById(R.id.imgResultadoImc);
+        txtAltura         = (EditText) findViewById (R.id.txtAltura      );
+        txtSituacaoPessoa = (TextView) findViewById (R.id.txtSituacaoPeso);
+        txtPeso           = (EditText) findViewById (R.id.txtPeso        );
+        imgResultadoImc   = (ImageView)findViewById (R.id.imgResultadoImc);
 
     }
 
-    public void calcularImc(View v) {
-        float peso = 0, altura = 0, imc = 0;
+    public void calculaImc(View v) {
+        float fPeso = 0, fAltura = 0, fImc = 0;
 
         if ((txtPeso.getText().toString().length() > 0) && (txtAltura.getText().toString().length() > 0))
         {
-            peso = Float.parseFloat(txtPeso.getText().toString());
-            altura = Float.parseFloat(txtAltura.getText().toString());
+            fPeso   = Float.parseFloat(txtPeso.getText().toString());
+            fAltura = Float.parseFloat(txtAltura.getText().toString());
 
-            if ((peso > 0) && (altura > 0))
+            if ((fPeso > 0) && (fAltura > 0))
             {
-                imc = (peso / (altura * altura));
-                if (imc <= 18.5)
+                fImc = (fPeso / (fAltura * fAltura));
+                if (fImc <= 18.5)
                 {
                     imgResultadoImc.setImageResource(R.drawable.abaixopeso);
                     txtSituacaoPessoa.setText("Abaixo do Peso");
-                } else if (imc <= 24.9)
+                } else if (fImc <= 24.9)
                 {
                     imgResultadoImc.setImageResource(R.drawable.normal);
                     txtSituacaoPessoa.setText("Peso Normal");
-                } else if (imc <= 29.9)
+                } else if (fImc <= 29.9)
                 {
                     imgResultadoImc.setImageResource(R.drawable.sobrepeso);
                     txtSituacaoPessoa.setText("SobrePeso");
-                } else if (imc <= 34.9)
+                } else if (fImc <= 34.9)
                 {
                     imgResultadoImc.setImageResource(R.drawable.obesidade1);
                     txtSituacaoPessoa.setText("Obesidade Tipo1");
-                } else if (imc <= 39.9)
+                } else if (fImc <= 39.9)
                 {
                     imgResultadoImc.setImageResource(R.drawable.obesidade2);
                     txtSituacaoPessoa.setText("Obesidade Tipo2");
